5fee8411a3e979e7a45bcea9c9f94d26874e2228
Emmanuel Villalobos <emmanuel.villalobos@gmail.com>
2017-10-21 12:10:57 -0400

    Angular setup with @angular/material, flex-layout and hammerjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
+import 'hammerjs'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +14,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
