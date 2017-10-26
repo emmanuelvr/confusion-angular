d4d45a09ee6781ed0bc294d283e61bd86f935411
Emmanuel Villalobos <emmanuel.villalobos@gmail.com>
2017-10-26 08:40:31 -0400

    Added dishes service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +27,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ DishService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
