7fd2919fcf401c7dd862591a300b3d522d0cadb7
Emmanuel Villalobos <emmanuel.villalobos@gmail.com>
2017-11-07 17:29:37 -0400

    Created header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser'
-import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
-import { MaterialModule } from '@angular/material'
-import { FlexLayoutModule } from '@angular/flex-layout'
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
-import 'hammerjs'
+import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
